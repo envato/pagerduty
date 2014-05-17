8907b11e1115243e7f6d2ab00aa904a172e5349e
Orien Madgwick <_@orien.io>
2014-05-17 14:40:53 +1000

    specs for Pagerduty#get_incident

--- spec/pagerduty_spec.rb
+++ spec/pagerduty_spec.rb
@@ -1,6 +1,21 @@
 # encoding: utf-8
 require "spec_helper"
 
+describe Pagerduty do
+  Given(:pagerduty) { Pagerduty.new(service_key) }
+  Given(:service_key) { "a-test-service-key" }
+
+  describe "#get_incident" do
+    Given(:incident_key) { "a-test-incident-key" }
+
+    When(:incident) { pagerduty.get_incident(incident_key) }
+
+    Then { incident.must_be_kind_of PagerdutyIncident }
+    Then { incident.service_key.must_equal service_key }
+    Then { incident.incident_key.must_equal incident_key }
+  end
+end
+
 describe PagerdutyException do
   Given(:pagerduty_instance) { mock }
   Given(:api_response) { mock }
@@ -9,8 +24,4 @@ describe PagerdutyException do
 
   Then { pagerduty_exception.pagerduty_instance == pagerduty_instance }
   Then { pagerduty_exception.api_response == api_response }
-end
-
-describe Pagerduty do
-
 end
