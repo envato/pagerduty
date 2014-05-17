5b2f93a1dd7c581f485e02b2ae5065c353f942cc
Orien Madgwick <_@orien.io>
2014-05-18 01:31:00 +1000

    Extract HTTP logic and settings into a dedicated module

---
+++ lib/pagerduty/http_transport.rb
@@ -0,0 +1,40 @@
+# encoding: utf-8
+require 'json'
+require 'net/http'
+require 'net/https'
+
+module Pagerduty::HttpTransport
+  class << self
+
+    HOST = "events.pagerduty.com"
+    PORT = 443
+    PATH = "/generic/2010-04-15/create_event.json"
+
+    def send(payload = {})
+      response = post payload.to_json
+      response.error! unless transported?(response)
+      JSON.parse(response.body)
+    end
+
+  private
+
+    def post(payload)
+      post = Net::HTTP::Post.new(PATH)
+      post.body = payload
+      http.request(post)
+    end
+
+    def http
+      http = Net::HTTP.new(HOST, PORT)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      http.ca_path = "/etc/ssl/certs"
+      http.verify_depth = 5
+      http
+    end
+
+    def transported?(response)
+      response.kind_of? Net::HTTPSuccess or response.kind_of? Net::HTTPRedirection
+    end
+  end
+end
