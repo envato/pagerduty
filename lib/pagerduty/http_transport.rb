ad54cb4e8f7e5497f7cc3d15ceb1792c119b1f41
Orien Madgwick <_@orien.io>
2014-05-22 22:50:25 +1000

    TIL extend self

--- lib/pagerduty/http_transport.rb
+++ lib/pagerduty/http_transport.rb
@@ -4,37 +4,36 @@ require 'net/http'
 require 'net/https'
 
 module Pagerduty::HttpTransport
-  class << self
+  extend self
 
-    HOST = "events.pagerduty.com"
-    PORT = 443
-    PATH = "/generic/2010-04-15/create_event.json"
+  HOST = "events.pagerduty.com"
+  PORT = 443
+  PATH = "/generic/2010-04-15/create_event.json"
 
-    def send(payload = {})
-      response = post payload.to_json
-      response.error! unless transported?(response)
-      JSON.parse(response.body)
-    end
+  def send(payload = {})
+    response = post payload.to_json
+    response.error! unless transported?(response)
+    JSON.parse(response.body)
+  end
 
-  private
+private
 
-    def post(payload)
-      post = Net::HTTP::Post.new(PATH)
-      post.body = payload
-      http.request(post)
-    end
+  def post(payload)
+    post = Net::HTTP::Post.new(PATH)
+    post.body = payload
+    http.request(post)
+  end
 
-    def http
-      http = Net::HTTP.new(HOST, PORT)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-      http.open_timeout = 60
-      http.read_timeout = 60
-      http
-    end
+  def http
+    http = Net::HTTP.new(HOST, PORT)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.open_timeout = 60
+    http.read_timeout = 60
+    http
+  end
 
-    def transported?(response)
-      response.kind_of? Net::HTTPSuccess or response.kind_of? Net::HTTPRedirection
-    end
+  def transported?(response)
+    response.kind_of? Net::HTTPSuccess or response.kind_of? Net::HTTPRedirection
   end
 end
