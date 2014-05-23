958828880918c30c350c4bccbb483e57eb4a4df9
Orien Madgwick <_@orien.io>
2014-05-23 21:14:53 +1000

    Describe some parameters

--- lib/pagerduty.rb
+++ lib/pagerduty.rb
@@ -14,10 +14,17 @@ class Pagerduty
 
   attr_reader :service_key
 
+  # @param [String] service_key The GUID of one of your "Generic API" services.
+  #   This is the "service key" listed on a Generic API's service detail page.
   def initialize(service_key)
     @service_key = service_key
   end
 
+  # @param [String] description A short description of the problem that led to
+  #   this trigger. This field (or a truncated version) will be used when
+  #   generating phone calls, SMS messages and alert emails. It will also appear
+  #   on the incidents tables in the PagerDuty UI. The maximum length is 1024
+  #   characters.
   def trigger(description, options = {})
     resp = api_call("trigger", options.merge(:description => description))
     ensure_success(resp)
@@ -53,15 +60,23 @@ end
 class PagerdutyIncident < Pagerduty
   attr_reader :incident_key
 
+  # @param [String] service_key The GUID of one of your "Generic API" services.
+  #   This is the "service key" listed on a Generic API's service detail page.
+  #
+  # @param [String] incident_key The unique identifier for the incident.
   def initialize(service_key, incident_key)
     super service_key
     @incident_key = incident_key
   end
 
+  # @param [String] description Text that will appear in the incident's log
+  #   associated with this event.
   def acknowledge(description = nil, details = nil)
     modify_incident("acknowledge", description, details)
   end
 
+  # @param [String] description Text that will appear in the incident's log
+  #   associated with this event.
   def resolve(description = nil, details = nil)
     modify_incident("resolve", description, details)
   end
