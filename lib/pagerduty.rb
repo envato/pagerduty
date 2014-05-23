3e79b9e05ccd73d98c2ec7b8ec72341bcbb7cd38
Orien Madgwick <_@orien.io>
2014-05-23 21:38:29 +1000

    Describe the optional trigger parameters

--- lib/pagerduty.rb
+++ lib/pagerduty.rb
@@ -16,6 +16,7 @@ class Pagerduty
 
   # @param [String] service_key The GUID of one of your "Generic API" services.
   #   This is the "service key" listed on a Generic API's service detail page.
+  #
   def initialize(service_key)
     @service_key = service_key
   end
@@ -25,6 +26,24 @@ class Pagerduty
   #   generating phone calls, SMS messages and alert emails. It will also appear
   #   on the incidents tables in the PagerDuty UI. The maximum length is 1024
   #   characters.
+  #
+  # @option options [String] :incident_key Identifies the incident to which
+  #   this trigger event should be applied. If there's no open (i.e. unresolved)
+  #   incident with this key, a new one will be created. If there's already an
+  #   open incident with a matching key, this event will be appended to that
+  #   incident's log. The event key provides an easy way to "de-dup" problem
+  #   reports. If this field isn't provided, PagerDuty will automatically open a
+  #   new incident with a unique key.
+  #
+  # @option options [String] :client The name of the monitoring client that is
+  #   triggering this event.
+  #
+  # @option options [String] :client_url The URL of the monitoring client that
+  #   is triggering this event.
+  #
+  # @option options [Hash] :details An arbitrary hash containing any data you'd
+  #   like included in the incident log.
+  #
   def trigger(description, options = {})
     resp = api_call("trigger", options.merge(:description => description))
     ensure_success(resp)
@@ -64,6 +83,7 @@ class PagerdutyIncident < Pagerduty
   #   This is the "service key" listed on a Generic API's service detail page.
   #
   # @param [String] incident_key The unique identifier for the incident.
+  #
   def initialize(service_key, incident_key)
     super service_key
     @incident_key = incident_key
@@ -71,12 +91,20 @@ class PagerdutyIncident < Pagerduty
 
   # @param [String] description Text that will appear in the incident's log
   #   associated with this event.
+  #
+  # @param [Hash] details An arbitrary hash containing any data you'd like
+  #   included in the incident log.
+  #
   def acknowledge(description = nil, details = nil)
     modify_incident("acknowledge", description, details)
   end
 
   # @param [String] description Text that will appear in the incident's log
   #   associated with this event.
+  #
+  # @param [Hash] details An arbitrary hash containing any data you'd like
+  #   included in the incident log.
+  #
   def resolve(description = nil, details = nil)
     modify_incident("resolve", description, details)
   end
