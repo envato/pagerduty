21e9b2171593d664a60fa3357c954374c485ad3b
Orien Madgwick <_@orien.io>
2020-04-10 17:28:27 +1000

    Resolve error handling defects
    And provide test coverage

---
+++ spec/pagerduty_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Pagerduty do
+  describe ".build" do
+    When(:build) { Pagerduty.build(config) }
+
+    context "given an integration key and API version: `1`" do
+      Given(:config) { { integration_key: "test-key", api_version: 1 } }
+      Then { expect(build).to be_a(Pagerduty::EventsApiV1) }
+    end
+
+    context "given an integration key and API version: `'1'`" do
+      Given(:config) { { integration_key: "test-key", api_version: "1" } }
+      Then { expect(build).to be_a(Pagerduty::EventsApiV1) }
+    end
+
+    context "given an integration key, but no API version" do
+      Given(:config) { { integration_key: "test-key" } }
+      Then {
+        expect(build).to have_raised(ArgumentError,
+                                     "api_version not provided")
+      }
+    end
+
+    context "given an API version, but no integration key" do
+      Given(:config) { { api_version: 1 } }
+      Then {
+        expect(build).to have_raised(ArgumentError,
+                                     "integration_key not provided")
+      }
+    end
+
+    context "given an integration key and an unknown API version" do
+      Given(:config) { { integration_key: "test-key", api_version: 0 } }
+      Then {
+        expect(build).to have_raised(ArgumentError,
+                                     "api_version 0 not supported")
+      }
+    end
+  end
+end
