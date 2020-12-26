776c10cf6a942ca560e6499b13dba639ec6d3a31
Orien Madgwick <_@orien.io>
2020-12-26 21:17:58 +1100

    Enable warnings on Ruby 2.7.2 and later

--- spec/support/warnings.rb
+++ spec/support/warnings.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7.2")
+  Warning[:deprecated] = true
+end
+
 module Warnings
   def self.silenced(&block)
     with_flag(nil, &block)
